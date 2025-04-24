b6f411d35afb7f378a193595e4f80d27d64dc34f
HHVinh <vinh0986585015@gmail.com>
2025-04-24 21:42:15 +0700

    Learn quickSort tiếp theo.cpp

--- quickSort2.cpp
+++ quickSort2.cpp
@@ -59,6 +59,29 @@ void quickSort2(int *arr, int l, int r){
     }
 }
 
+int phanHoachGiua(int *arr, int left, int right){
+    int pivot = arr[(left+right) / 2];
+    int l = left, r = right;
+
+    while(1){
+        while(arr[l] < pivot) l++;
+        while(arr[r] > pivot) r--;
+        if(l >= r) break;
+        swap(&arr[l], &arr[r]);
+        l++;
+        r--;
+    }
+    return r;
+}
+
+void quickSort3(int *arr, int l, int r){
+    if(l < r){
+        int mid = phanHoachGiua(arr,l,r);
+        quickSort3(arr,l,mid);
+        quickSort3(arr,mid + 1, r);
+    }
+}
+
 int main(){
     int n = 10;
     int arr1[10] = {23, 5, 17, 1, 42, 8, 31, 3, 19, 12};
@@ -73,9 +96,9 @@ int main(){
     printf("\nMảng Pivot Phải vừa sắp xếp là: \n");
     xuatMang(arr2, n);
 
-    // quickSort3(arr3,0,n-1);
-    // printf("\nMảng Pivot Giữa vừa sắp xếp là: \n");
-    // xuatMang(arr3, n);
+    quickSort3(arr3,0,n-1);
+    printf("\nMảng Pivot Giữa vừa sắp xếp là: \n");
+    xuatMang(arr3, n);
 
     return 0;
 }
